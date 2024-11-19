c9fd9d7314ada6d8f3fae10bdb540215d33e1904
Oleg <neevidd@yandex.ru>
2024-11-19 17:02:31 +0300

    ttl

--- auth/sso/internal/presentation/grpc/config/config.go
+++ auth/sso/internal/presentation/grpc/config/config.go
@@ -3,6 +3,7 @@ package config
 import "time"
 
 type GRPCConfig struct {
-	Port    int           `yaml:"port"`
-	Timeout time.Duration `yaml:"timeout"`
+	Port     int           `yaml:"port"`
+	Timeout  time.Duration `yaml:"timeout"`
+	TokenTTL time.Duration `yaml:"token_ttl"`
 }
