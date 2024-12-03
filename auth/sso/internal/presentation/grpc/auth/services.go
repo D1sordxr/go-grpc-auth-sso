75f38e66e19e9fd24b1173797db8fb43a845d8c8
Oleg <neevidd@yandex.ru>
2024-12-04 01:21:28 +0300

    msg

--- auth/sso/internal/presentation/grpc/auth/services.go
+++ auth/sso/internal/presentation/grpc/auth/services.go
@@ -21,15 +21,16 @@ func NewUserAuthService() *UserAuthService {
 
 func (s *UserAuthService) Register(ctx context.Context, req *services.RegisterRequest) (*services.RegisterResponse, error) {
 	// TODO: Реализовать логику регистрации
-	panic("Register method not implemented")
+	return &services.RegisterResponse{}, nil
 }
 
 func (s *UserAuthService) Login(ctx context.Context, req *services.LoginRequest) (*services.LoginResponse, error) {
 	// TODO: Реализовать логику логина
-	panic("Login method not implemented")
+	return &services.LoginResponse{}, nil
+
 }
 
 func (s *UserAuthService) IsAdmin(ctx context.Context, req *services.IsAdminRequest) (*services.IsAdminResponse, error) {
 	// TODO: Реализовать проверку админских прав
-	panic("IsAdmin method not implemented")
+	return &services.IsAdminResponse{}, nil
 }
